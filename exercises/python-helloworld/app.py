c54a793fca38a83b565db960b48acc9983acc590
Juan Tellez <juan.tellez@aa.com>
2021-06-12 00:27:54 -0500

    adding health check and metrics endpoints

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,40 @@
-from flask import Flask
+from flask import Flask, jsonify
+import json
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+
+@app.route("/status")
+def healthCheck():
+    response = {
+        "result": "OK - healthy"
+    }
+    return app.response_class(
+        response=json.dumps(response),
+        status=200,
+        mimetype='application/json'
+    )
+
+
+@app.route("/metrics")
+def metrics():
+    response = {
+        "data": {
+            "UserCount": 140,
+            "UserCountActive": 23
+        }
+    }
+    return app.response_class(
+        response=json.dumps(response),
+        status=200,
+        mimetype='application/json'
+    )
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
